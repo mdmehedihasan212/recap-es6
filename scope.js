08b2db4de37162ced4ef16ae78fa6ea2e9ded678
mdmehedihasan212 <mdmehedihasan384@gmail.com>
2022-03-04 00:30:18 +0600

    scope

---
+++ scope.js
@@ -0,0 +1,19 @@
+const baperTaka = 100000;
+function patriChi(age, earning) {
+    console.log(trueAge);
+    console.log(age);
+    console.log(earning);
+
+    var trueAge = 7;
+}
+
+function canMarry(expense) {
+    const total = baperTaka + earning - expense;
+    if (total > 1000) {
+        const showing = 5666;
+        console.log(showing);
+        return true;
+    }
+    return false;
+}
+patriChi(34, 8000)
